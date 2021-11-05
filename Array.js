2360dcbc70d9fa8229723da428d9e7c908eac6a6
Sayfiddin4353 <sayfiddin.gofurov@inbox.ru>
2021-11-05 14:45:45 +0500

    test

---
+++ Array.js
@@ -0,0 +1,61 @@
+// 1-masala
+
+// var fruit = ['apple', 12, 'Ananas', 'Orange', 'Banana'];
+// var count = 0;
+// var a = fruit.join('');
+// console.log(a);
+// for (i = 0; i < a.length; i++){
+//     if (a[i] == 'a' || a[i] == 'A') {
+        
+//         count++;
+//     }
+// }
+
+// console.log(count, 'ta a harfi bor');
+
+// 2-masala
+
+// var number = [10, 5, 11, 'apple', 2, 3, 4, 5, 6];
+// var sum = 0;
+
+// let rest = number.filter((value) => {
+//     return typeof value ==='number'
+// });
+// // console.log(rest)
+
+// for (i = 0; i < rest.length; i++){
+//     sum = sum + rest[i];
+// }
+// console.log(sum);
+
+
+// 3-masala
+
+// var number = [10, 5, 11, 'apple', 2, 3, 4, 5, 6];
+
+// var a = 0;
+// var b = 0;
+//    let rest = number.filter((value) => {
+//     return typeof value ==='number'
+//    });
+// rest = rest.sort((a, b) => a - b);
+// console.log(rest);
+
+// a = rest[1];
+// b = rest[rest.length - 2];
+
+// console.log("Eng kichigining bitta kattasi = ", a);
+// console.log("Eng kattasining bitta kichigi = ", b);
+  
+
+// 4-masala
+
+// var number = [10, 5, 11, 2, 3, 4, 5, 6];
+// for (i = 0; i <Text.length; i++){
+    
+    
+// }
+// console.log(number);
+
+
+
