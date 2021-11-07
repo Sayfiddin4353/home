3a3aa7b0ea8b0757855cfd04cc7e7caa625f9ee9
Sayfiddin4353 <sayfiddin.gofurov@inbox.ru>
2021-11-08 00:08:25 +0500

    test

--- Array.js
+++ Array.js
@@ -6,7 +6,7 @@
 // console.log(a);
 // for (i = 0; i < a.length; i++){
 //     if (a[i] == 'a' || a[i] == 'A') {
-        
+
 //         count++;
 //     }
 // }
@@ -28,7 +28,6 @@
 // }
 // console.log(sum);
 
-
 // 3-masala
 
 // var number = [10, 5, 11, 'apple', 2, 3, 4, 5, 6];
@@ -46,16 +45,80 @@
 
 // console.log("Eng kichigining bitta kattasi = ", a);
 // console.log("Eng kattasining bitta kichigi = ", b);
-  
 
 // 4-masala
 
 // var number = [10, 5, 11, 2, 3, 4, 5, 6];
-// for (i = 0; i <Text.length; i++){
-    
+// juft = number.filter((value, index) => index % 2 == 0);
+// toq = number.filter((value, index) => index % 2 == 1);
+// sum = 0;
+// console.log(juft);
+// console.log(toq);
+// console.log(juft.reduce((a, b) => a + b));
+// console.log(toq.reduce((a, b) => a + b));
+
+
+// var number = [10, 5, 11, 2, 3, 4, 5, 6];
+// var juft = [];
+// var toq = [];
+// for (i = 0; i < number.length; i++){
+//     if (i % 2 == 0) {
+//         juft.push(number[i]);
+//     } else {
+//         toq .push(number[i]);
+//     }
     
 // }
-// console.log(number);
+// console.log(juft);
+// console.log(toq);
 
+// Array elementlarini teskarisiga chiqarish.
+
+// var number = [10, 5, 11, 2, 3, 4, 5, 6];
+// let newar = [];
+// for (i = number.length-1; i >= 0; i--){
+//      newar.push(number[i]);
+// }
+// console.log(newar);
+
+// Filter ni metoddan foydalanmay chiqarish
+
+// Number uchun
+// var number = [10, 5, 11, 'string ','salom',2, 3, 4, 5, 6];
+// var newar = [];
+// for (i = 0; i < number.length; i++){
+//     if (  number[i]>=6 ) {
+//         newar.push(number[i]);
+//     }
+// }
+// console.log(number)
+// console.log(newar)
+
+// funksiyadan foydalanish
+var number = [10, 5, 11, 'string ','salom',2, 3, 4, 5, 6];
+
+// const sarala = (array) => {
+// var newar = [];
+// for (i = 0; i < array.length; i++){
+//     if (  array[i]>=6 ) {
+//         newar.push(array[i]);
+//     }
+// }
+//     return newar
+// }
+
+// console.log(sarala(number))
+
+// String uchun
+
+// var fruit = ['apple',  'Ananas', 'Orange', 'Banana'];
+// var newfruit = [];
+// for (i = 0; i < fruit.length; i++){
+//     if ( fruit[i] ===  fruit[i].toUpperCase()) {
+//         newfruit.push(fruit[i]);
+//     }
+// }
+// console.log(newfruit);
+// console.log(string);
 
 
